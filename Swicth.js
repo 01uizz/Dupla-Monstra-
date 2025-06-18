d83fcd21041d0393191b684895e21992809b5a98
Yan Nunes <yanunesxz@gmai.com>
2025-06-18 14:56:52 -0300

    Teste de integração com GitHub

---
+++ Swicth.js
@@ -0,0 +1,39 @@
+let = require('prompt-sync')();
+
+let opcao = parseFloat(prompt('Digite uma opção de 1 a 4: '));
+let usernumero = parseFloat(prompt('Escolha a tabuada: '));
+
+switch (opcao) {
+  case 1: 
+    console.log("Opção Somar");
+    for (let i = 1; i <= 10; i++) {
+      console.log(usernumero + ' + ' + i + ' = ' + (usernumero + i));
+    }
+    break;
+
+  case 2: 
+    console.log("Opção Subtrair");
+    for (let i = 1; i <= 10; i++) {
+      console.log(usernumero + ' - ' + i + ' = ' + (usernumero - i));
+    }
+    break;
+
+  case 3: 
+    console.log("Opção Multiplicar");
+    for (let i = 1; i <= 10; i++) {
+      console.log(usernumero + ' * ' + i + ' = ' + (usernumero * i));
+    }
+    break;
+
+  case 4: 
+    console.log("Opção Divisão");
+    for (let i = 1; i <= 10; i++) {
+      console.log(usernumero + ' / ' + i + ' = ' + (usernumero / i));
+    }
+    break;
+
+  default:
+    console.log("Opção inválida. Digite um número de 1 a 4."); }
+
+    {}
+    
